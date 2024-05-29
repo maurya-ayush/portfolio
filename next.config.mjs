8a00dad46a6a32eb8a7b6011ef38c28a4907f53d
maurya-ayush <ayushmaurya2468@gmail.com>
2024-05-30 00:53:27 +0530

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
+    basePath: "/2048-in-react",
+    output: "export",
     reactStrictMode: true,
 };
 
